705b4cf214e3531e56977aafaf753663a8c78160
Ryan LaBouve <imryanlabouve@gmail.com>
2016-11-06 00:15:04 -0500

    Initial spike

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* jshint node: true */
 'use strict';
 
+var open = require('open');
+var fs = require('fs');
+
+var openFn = function() {
+  var defaultPort = 4200;
+  var configFile = fs.readFileSync('.ember-cli', 'utf8');
+  var config = JSON.parse(configFile);
+  var url = 'http://localhost:' + config.port || defaultPort;
+  console.log('Opening your project on ' + url);
+  open(url);
+};
+
 module.exports = {
-  name: 'ember-cli-o'
+  name: 'ember-cli-o',
+  includedCommands: function() {
+    return {
+      oCommand: {
+        name: 'o',
+        description: 'Opens your ember project',
+        run: openFn
+      },
+      openCommand: {
+        name: 'open',
+        description: 'Opens your ember project',
+        run: openFn
+      }
+    }
+  }
 };
